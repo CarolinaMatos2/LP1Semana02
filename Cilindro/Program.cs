021b19b6ce2709efa090631022699292bfafb2d7
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-02-29 21:07:25 +0000

    Add height and radius

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,10 +7,12 @@ namespace Cilindro
         static void Main(string[] args)
         {
             Console.Write("Enter the height of the cylinder: ");
+            float height = float.Parse(Console.ReadLine());
+            Console.Write("Enter the radius of the cylinder: ");
+            float radius = float.Parse(Console.ReadLine());
+
             
 
-            Console.Write("Enter the radius of the cylinder: ");
-            
         }
     }
 }
