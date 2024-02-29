cbaa997ef0b05b48ebd8a972d7f84498368579f7
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-02-29 19:35:59 +0000

    Little adjustments 2

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -9,7 +9,7 @@ namespace SmoothOperator
             Console.Write("Enter a non-negative integer: ");
             string input = Console.ReadLine();
 
-            byte n = (byte)(int.TryParse(input) && input >= 0);
+            byte n = (int.Parse(input) && input >= 0);
             Console.WriteLine($"Resultado da operação de decremento de {n}: {--n}");
             Console.WriteLine($"Resultado da operação de incremento de {n}: {++n}");
 
