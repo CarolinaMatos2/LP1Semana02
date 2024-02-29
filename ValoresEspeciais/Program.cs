fc81fcf5d6d9c29c18ec8cb9eb27ef7b490c856d
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-02-29 22:44:35 +0000

    Add Maximum and minimum values of all types

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,57 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Maximum and minimum values of all types
+            sbyte sb1 = sbyte.MaxValue;
+            sbyte sb2 = sbyte.MinValue;
+            Console.WriteLine(sb1);
+            Console.WriteLine(sb2);
+
+            int i1 = int.MaxValue;
+            int i2 = int.MinValue;
+            Console.WriteLine(i1);
+            Console.WriteLine(i2);
+
+            uint ui1 = uint.MaxValue;
+            uint ui2 = uint.MinValue;
+            Console.WriteLine(ui1);
+            Console.WriteLine(ui2);
+
+            ulong u1 = ulong.MaxValue;
+            ulong u2 = ulong.MinValue;
+            Console.WriteLine(u1);
+            Console.WriteLine(u2);
+
+            long l1 = long.MaxValue;
+            long l2 = long.MinValue;
+            Console.WriteLine(l1);
+            Console.WriteLine(l2);
+
+            double d1 = double.MaxValue;
+            double d2 = double.MinValue;
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+
+            byte b1 = byte.MaxValue;
+            byte b2 = byte.MinValue;
+            Console.WriteLine(b1);
+            Console.WriteLine(b2);
+
+            short s1 = short.MaxValue;
+            short s2 = short.MinValue;
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+            ushort us1 = ushort.MaxValue;
+            ushort us2 = ushort.MinValue;
+            Console.WriteLine(us1);
+            Console.WriteLine(us2);
+
+            char c1 = char.MaxValue;
+            char c2 = char.MinValue;
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            
         }
     }
 }
