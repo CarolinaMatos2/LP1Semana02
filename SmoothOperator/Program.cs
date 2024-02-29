30c1ed1fda6eae8e65ad84e2d8672b8dd5522779
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-02-29 20:40:43 +0000

    Exercise 4 adjustment

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -12,7 +12,10 @@ namespace SmoothOperator
             byte n = (int.Parse(input) && input >= 0);
             Console.WriteLine($"Resultado da operação de decremento de {n}: {--n}");
             Console.WriteLine($"Resultado da operação de incremento de {n}: {++n}");
-
+            Console.WriteLine($"Divisão por 2 de {n}: {n/2}");
+            Console.WriteLine($"Shift left por 3 de {n}: {n<<3}");
+            Console.WriteLine($"Xor com 5 de {n}: {n^5}");
+            Console.WriteLine($"É maior que 10? {n>10}");
         }
     }
 }
