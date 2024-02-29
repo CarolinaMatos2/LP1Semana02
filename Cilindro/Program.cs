4fdffdf20b235f2ad6b9833e06762743f8317dfb
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-02-29 21:48:14 +0000

    Add a F on MathF.PI

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -11,8 +11,8 @@ namespace Cilindro
             Console.Write("Enter the radius of the cylinder: ");
             float radius = float.Parse(Console.ReadLine());
 
-            float V = Math.PI * (float)Math.Pow(radius, 2) * height;
-            float Sa = 2 * Math.PI * radius * (radius + height);
+            float V = MathF.PI * (float)MathF.Pow(radius, 2) * height;
+            float Sa = 2 * MathF.PI * radius * (radius + height);
 
             Console.WriteLine($"The volume of the cylinder {V:F2}");
             Console.WriteLine($"The surface area of the cylinder {Sa:F2}");
