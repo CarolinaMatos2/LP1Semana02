1b9c8bfa174582677c5fc452143a43acbad97879
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-03-01 01:46:39 +0000

    Add Asks the user a question

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,7 +6,12 @@ namespace ChatGPTOne
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.Write("Pergunte alguma coisa, se não, digite Exit para sair: "); //Solicita ao utilizador uma questão
+                string input = Console.ReadLine();
+                
+            }
         }
     }
 }
