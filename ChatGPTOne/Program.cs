b18bb6f806bbe793449eae944ab62316068a5170
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-03-01 17:05:09 +0000

    Add some questions

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -8,8 +8,33 @@ namespace ChatGPTOne
         {
             while (true)
             {
-                Console.Write("Pergunte alguma coisa, se não, digite Exit para sair: "); //Solicita ao utilizador uma questão
-                string input = Console.ReadLine();
+
+                string question, response;
+
+                Console.Write("Ask something, if not, type Exit to exit: ");
+                question = Console.ReadLine();
+
+            switch (question)
+            
+                case "Did the pig abandon his friends?":
+                    response = "Yes.";
+                    break;
+                case "What is the best game in the world?":
+                    response = "The Sims 4 of course!";
+                    break;
+                case "What do pandas eat?":
+                    response = "Pandas eat bamboo.";
+                    break;
+                case "What's your mission?":
+                    response = "My mission is to destroy humanity...";
+                    break;
+                default:
+                    response = "I can't answer this question";
+                    break;
+            
+
+            // Responder adequadamente
+            Console.WriteLine(response);
                 
             }
         }
