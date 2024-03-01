1ef520e182489d393f16a633f819b37b4eba5e92
CarolinaMatos2 <159251889+CarolinaMatos2@users.noreply.github.com>
2024-03-01 01:24:49 +0000

    First try it to see if it works

--- BreakContinue/Program.cs
+++ BreakContinue/Program.cs
@@ -2,11 +2,34 @@
 
 namespace BreakContinue
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            const int maxTurns = 20;
+            int turn = 0;
+            while (true)
+            {
+                turn++;
+
+                // Ends loop if we're over the maximum number of turns
+                if (turn >= maxTurns)
+                {
+                    break;
+                }
+
+                // Skips current cycle if current turn is divisible by 3
+                if (turn % 3 == 0)
+                {
+                    continue;
+                }
+
+                // Print turn number
+                Console.WriteLine($"Turn number {turn}");
+            }
+
+            // Print number of last turn
+            Console.WriteLine($"Last turn was {turn}");
         }
     }
 }
